28a29c16d73fccab104c8df45d0017116a1dfeba
Eli Edds <eliedds@gmail.com>
2019-07-11 11:44:04 -0400

    added sign in, communicates with googl form, then GAS that emails to email in printerID, if it is a @cpsfc.org email

--- app.js
+++ app.js
@@ -9,19 +9,36 @@ const context = canvas.getContext('2d');
 var cropper;
 
 var croppedImage;
+var previewed;
+var svg;
 
 var generateSVG;
 
+var svgParams = {};
+
+function printCanvas(dataUrl) {
+  var windowContent = '<!DOCTYPE html>';
+  windowContent += '<html>'
+  windowContent += '<head><title>Print canvas</title></head>';
+  windowContent += '<body>'
+  windowContent += '<img src="' + dataUrl + '">';
+  windowContent += '</body>';
+  windowContent += '</html>';
+  var printWin = window.open('', '', 'width=340,height=260');
+  printWin.document.open();
+  printWin.document.write(windowContent);
+  printWin.document.close();
+  printWin.focus();
+  printWin.print();
+  printWin.close();
+}
+
 function auth() {
-  //if (gapi.auth == undefined)
-  //  gapi.load('client', auth);
-
-  gapi.auth.authorize({
-    'client_id': '427410813651-k8tckputmdbhg890vp7fap5unh5rdlg5.apps.googleusercontent.com',
-    'scope': 'https://www.googleapis.com/auth/cloudprint',
-    'immediate': true
+  $.getJSON("priv_service_account.json", function (json) {
+    gapi.auth(json.client_email, "https://www.googleapis.com/auth/cloudprint https://www.googleapis.com/auth/drive", json.private_key, (success) => {
+      console.log("Authenticated");
+    });
   });
-
 }
 
 function print() {
@@ -51,7 +68,7 @@ function print() {
 
 }
 
-window.addEventListener('load', auth);
+$(document).ready(auth);
 
 const constraints = {
   video: true,
@@ -107,6 +124,11 @@ $("#capture").click(() => {
 
   $("#video-modal").addClass("hidden");
   $("#preview-modal").removeClass("hidden");
+
+  $("#preview").text("Crop Picture");
+  croppedImage = false;
+  previewed = false;
+  svg = undefined;
 });
 
 $("#retake").click(() => {
@@ -115,93 +137,116 @@ $("#retake").click(() => {
 });
 
 $("#canvas").click(() => {
-  console.log(canvas.toDataURL());
 })
 
 $("#preview").click(() => {
+  if (previewed) {
+    //TODO: Print
+    var params = JSON.stringify(svgParams);
 
-  if (!croppedImage) {
-    croppedImage = true;
-    $("#preview").text("Preview ID");
+    let demoSVG = `<?xml version="1.0" standalone="no"?>
+<svg width="160" height="140" xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <line x1="40" x2="120" y1="20" y2="20" stroke="black" stroke-width="20" stroke-linecap="butt"/>
+  <line x1="40" x2="120" y1="60" y2="60" stroke="black" stroke-width="20" stroke-linecap="square"/>
+  <line x1="40" x2="120" y1="100" y2="100" stroke="black" stroke-width="20" stroke-linecap="round"/>
+</svg>`
 
-    let factor = 1 / 3;
-    var width = 1030 * factor;
-    var height = 1283 * factor;
+    var doc = new jsPDF({
+      orientation: 'p',
+      unit: 'px',
+      format: [353, 504]
+    })
+    doc.addSvgAsImage(demoSVG, 0, 0, 353, 504, "demoSVG", "NONE", 0)
 
-    cropper = new Cropper(canvas, {
-      viewMode: 2,
-      dragMode: 'move',
-      aspectRatio: width / height,
-      movable: false,
-      rotatable: false,
-      scalable: false,
-      zoomable: false,
-      cropBoxResizable: true,
-      data: {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
-        width: width,
-        height: height,
-        rotate: 0,
-        scaleX: 1,
-        scaleY: 1
-      },
-      crop(event) {
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
-      },
+    var pdf = doc.output('datauri', { filename: "id.pdf" });
+
+    gapi.drive.upload("ID: " + svgParams.name, svg, "image/svg+xml", (success, id) => {
+      if (!success) {
+        console.error("Error on File Upload");
+      } else {
+        gapi.print.submit(id);
+      }
     });
-  } else {
-    var img;
-    if (cropper) {
-      img = cropper.getCroppedCanvas().toDataURL();
-      cropper.destroy();
+  } else
+    if (!croppedImage) {
+      croppedImage = true;
+      $("#preview").text("Preview ID");
+
+      let factor = 1 / 3;
+      var width = 1030 * factor;
+      var height = 1283 * factor;
+
+      cropper = new Cropper(canvas, {
+        viewMode: 2,
+        dragMode: 'move',
+        aspectRatio: width / height,
+        movable: false,
+        rotatable: false,
+        scalable: false,
+        zoomable: false,
+        cropBoxResizable: true,
+        data: {
+          x: canvas.width / 2,
+          y: canvas.height / 2,
+          width: width,
+          height: height,
+          rotate: 0,
+          scaleX: 1,
+          scaleY: 1
+        },
+        crop(event) {
+        },
+      });
     } else {
-      console.error("ERROR: Cropper Does not Exist, cannot make ID")
+      var img;
+      if (cropper) {
+        img = cropper.getCroppedCanvas().toDataURL();
+        cropper.destroy();
+      } else {
+        console.error("ERROR: Cropper Does not Exist, cannot make ID")
+      }
+      console.log("Generating ID...");
+      var formArray = $("#form").serializeArray();
+      var form = {};
+
+      formArray.forEach((obj) => {
+        form[obj.name] = obj.value;
+      });
+      var name = form["name"];
+
+      var now = new Date();
+      var reason = form["reason"] + " | " + now.getMonth() + "/" + now.getDay() + "/" + now.getFullYear().toString().substr(2, 2);
+
+      var dest = "To: " + form["dest"];
+
+      svg = generateSVG(dest, reason, name, img);
+
+      $("#canvas").attr("height", 353).attr("width", 504);
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      var img = new Image();
+      img.onload = function () {
+        context.drawImage(img, 0, 0);
+      }
+
+      var url = 'data:image/svg+xml;base64, ' + btoa(svg);//URL.createObjectURL(blob);
+      let image = document.createElement('img');
+      img.src = url;
+
+      $("#preview").text("Print");
+      previewed = true;
+
+      //TODO: Print
+      //gapi.print.submit();
     }
-    console.log("Generating ID...");
-    var formArray = $("#form").serializeArray();
-    var form = {};
-    console.log(formArray);
-
-    formArray.forEach((obj) => {
-      console.log(obj);
-      form[obj.name] = obj.value;
-    });
-    var name = form["name"];
-    var reason = form["reason"];
-    var dest = "To: " + form["dest"];
-
-    var svg = generateSVG(dest, reason, name, img);
-
-    $("#canvas").attr("height", 353).attr("width", 504);
-
-    var url;
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    var img = new Image();
-    img.onload = function () {
-      context.drawImage(img, 0, 0);
-
-      //if (url) {
-      //  URL.revokeObjectURL(url)
-      //}
-    }
-
-    let blob = new Blob([svg], { type: 'image/svg+xml' });
-    url = URL.createObjectURL(blob);
-    let image = document.createElement('img');
-    img.src = url;
-  }
 })
 
 // Generate an ID in the SVG format for the given name, year, title and image (in a base 64 uri)
 generateSVG = function (name, year, title, image) {
+  svgParams.name = name;
+  svgParams.year = year;
+  svgParams.title = title;
+  svgParams.image = image;
   // Generate the name of the file, using the name field
   var id_name = "id_" + name.toLowerCase().replace(" ", "_") + ".svg";
   // Get the text of the template, stored as a .txt in google drive
@@ -214,6 +259,7 @@ generateSVG = function (name, year, title, image) {
     .replace("[[TITLE]]", title)
     // Insert the image into the template
     .replace("[[IMAGE]]", image);
+
 
   return svg;
 
--- gapi.js
+++ gapi.js
@@ -6,22 +6,206 @@
 
 var gapi = {};
 
-gapi._auth = { auth: false, access_token: "" }
+gapi._auth = { bAuth: false, access_token: "", exp: 0, cache: undefined }
 
-gapi.auth = function (client_id, client_secret, callback) {
-    if (gapi._auth) {
-        console.log("Already Authorized, returning...");
-        return false;
+gapi.refresh = function (callback) {
+    if (gapi._auth.cache == undefined) {
+        console.error("No Authentication Request Cache, cannot refresh token");
+        callback();
+        return;
+    } else {
+        var email = gapi._auth.cache.email;
+        var scope = gapi._auth.cache.scope;
+        var private_key_str = gapi._auth.cache.key;
+        gapi.auth(email, scope, private_key_str, callback);
+    }
+}
+
+gapi.auth = function (email, scope, private_key_str, callback) {
+    if (gapi._auth.bAuth) {
+        console.log("Already Authorized, checking expiration...");
+        if (KJUR.jws.IntDate.get("now") > gapi._auth.exp) {
+            console.log("Token Expired, Requesting New One...");
+            gapi._auth.bAuth = false;
+
+            gapi.auth(email, scope, private_key_str, callback);
+            return;
+        } else {
+            console.log("Token is still valid, skipping new request");
+            callback(false);
+            return;
+        }
     }
 
-    // Base64url of JWT Header
-    const header = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9";
-    var claim_set = {};
+    var cache = {};
 
-    claim_set.iss = "";
-    claim_set.scope = "";
-    claim_set.aud = "https://www.googleapis.com/oauth2/v4/token";
-    claim_set.exp = "";
-    claim_set.iat = "";
+    cache.email = email;
+    cache.scope = scope;
+    cache.key = private_key_str;
+    gapi._auth.cache = cache;
 
+    var pHeader = { "alg": "RS256", "typ": "JWT" }
+    var sHeader = JSON.stringify(pHeader);
+
+    var pClaim = {};
+    pClaim.aud = "https://www.googleapis.com/oauth2/v4/token";
+    pClaim.scope = scope;
+    pClaim.iss = email;
+    pClaim.exp = KJUR.jws.IntDate.get("now + 1hour");
+    pClaim.iat = KJUR.jws.IntDate.get("now");
+    //pClaim.sub = "eddsecp@cpsfc.org";
+
+    var sClaim = JSON.stringify(pClaim);
+
+    var sJWS = KJUR.jws.JWS.sign(null, sHeader, sClaim, private_key_str);
+
+    var XHR = new XMLHttpRequest();
+    var urlEncodedData = "";
+    var urlEncodedDataPairs = [];
+
+    urlEncodedDataPairs.push(encodeURIComponent("grant_type") + '=' + encodeURIComponent("urn:ietf:params:oauth:grant-type:jwt-bearer"));
+    urlEncodedDataPairs.push(encodeURIComponent("assertion") + '=' + encodeURIComponent(sJWS));
+    urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+    // We define what will happen if the data are successfully sent
+    XHR.addEventListener('load', function (event) {
+        if (XHR.readyState == 4) {
+            var response = JSON.parse(XHR.responseText);
+            console.log(response);
+            if (response.access_token) {
+                console.log("Requested Access Token Succesfully");
+                gapi._auth.exp = pClaim.exp;
+                gapi._auth.access_token = response.access_token;
+                gapi._auth.bAuth = true;
+                callback(true);
+                return;
+            }
+        } else {
+            console.error(XHR.responseText);
+            callback(false);
+            return;
+        }
+        //token = response["access_token"]
+    });
+
+    // We define what will happen in case of error
+    XHR.addEventListener('error', function (event) {
+        console.log('Oops! Something went wrong.');
+        callback(false);
+        return;
+    });
+
+    XHR.open('POST', 'https://www.googleapis.com/oauth2/v4/token');
+    XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    XHR.send(urlEncodedData)
+    return;
+}
+
+gapi.print = {};
+gapi.drive = {};
+
+gapi.print.submit = function (img) {
+    // Make sure we have an access token
+    gapi.refresh(() => {
+
+        var xhr = new XMLHttpRequest();
+        var printerID = "eddsecp@cpsfc.org"//Also Target Email, "b45bfeeb-46b2-2665-3169-738623ffd140"; // Ringo for Testing, change to proper printer on deploy
+        var title = "Test Print Job";
+        var data = img;
+        var contentType = "application/pdf";
+        var tags = "Automated; Sign In; ID";
+
+        //formData.append("", "drunknight");
+        //var url = 'https://www.google.com/cloudprint/search';
+        var authStr = "Bearer " + gapi._auth.access_token;
+        var url = 'https://docs.google.com/forms/d/e/1FAIpQLScygIXCUFbpQGrZ7o6Tbiy-GIQQGprPeXnEYEdrjbqf5wKcRA/formResponse?usp=pp_url&entry.558004711=' + encodeURIComponent(authStr) + '&entry.1902462614=' + encodeURIComponent(printerID) + '&entry.1783159477=' + encodeURIComponent(title) + '&entry.1789236024=' + encodeURIComponent(data) + '&entry.1329547601=' + encodeURIComponent(contentType) + '&entry.886182819=' + encodeURIComponent(tags);
+        xhr.open('POST', url, true);
+        //formData.append("auth", "Bearer" + gapi._auth.access_token);
+
+        //xhr.setRequestHeader("Content-Type", undefined);//"application/x-www-form-urlencoded");
+        //xhr.setRequestHeader("Authorization", "Bearer" + gapi._auth.access_token);
+
+        xhr.addEventListener('load', function (e) {
+            console.error(xhr.responseText);
+        });
+
+        //console.log("submitted search request");
+
+        xhr.send();//formData);
+
+        //$.ajax({type: "POST", url: 'https://www.google.com/cloudprint/search', data: {}, success: (data, status) => {console.log(status);console.log(data);}});
+    });
+}
+
+// Name is for future compatibility, it is currently ignored
+gapi.drive.upload = function (name, data, contentType, callback) {
+    // Make sure we have an access token
+    gapi.refresh(() => {
+        var id;
+        $.ajax({
+            type: "POST",
+            url: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media',
+            data: data,
+            error: (xhr, textStatus, errorThrown) => {
+                console.log(errorThrown);
+                console.log(textStatus);
+                console.log(xhr.responseText);
+                callback(false);
+            },
+            contentType: contentType,
+            headers: {
+                Authorization: "Bearer " + gapi._auth.access_token
+            },
+            success: (data, status) => {
+                id = data.id;
+
+                let meta = {
+                    name: name
+                };
+
+                $.ajax({
+                    type: "PATCH",
+                    url: 'https://www.googleapis.com/drive/v3/files/' + id,
+                    data: JSON.stringify(meta),
+                    error: (xhr, textStatus, errorThrown) => {
+                        console.log(errorThrown);
+                        console.log(textStatus);
+                        console.log(xhr.responseText);
+                        callback(false);
+                    },
+                    headers: {
+                        Authorization: "Bearer " + gapi._auth.access_token
+                    },
+                    contentType: "application/json",
+                    success: (data, status) => {
+                        console.log(data);
+
+                        var permissions = {
+                            'type': 'anyone',
+                            'role': 'reader'
+                        }
+
+                        $.ajax({
+                            type: "POST",
+                            url: 'https://www.googleapis.com/drive/v3/files/' + id + '/permissions?sendNotificationEmail=false',
+                            data: JSON.stringify(permissions),
+                            error: (xhr, textStatus, errorThrown) => {
+                                console.log(errorThrown);
+                                console.log(textStatus);
+                                console.log(xhr.responseText);
+                                callback(false);
+                            },
+                            headers: {
+                                Authorization: "Bearer " + gapi._auth.access_token
+                            },
+                            contentType: "application/json",
+                            success: (data, status) => {
+                                callback(true, id);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
 }
